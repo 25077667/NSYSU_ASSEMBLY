9e04556c3c3a99bb85ca79fa0ea92119e60468e4
25077667 <zxc25077667@gmail.com>
2020-11-29 00:39:20 +0800

    Fix poly div bugs

--- driver/testPoly.c
+++ driver/testPoly.c
@@ -7,7 +7,7 @@
 
 void print_poly(const struct Poly p, const char *var_name)
 {
-    printf("This is poly %s: ", var_name);
+    printf("This is poly %s: \t", var_name);
     for (int i = p.degree; i >= 0; i--)
         printf("%dX^%d + ", p.coef[i], i);
     printf("\n");
@@ -31,14 +31,24 @@ int main(void)
         print_poly(a, GET_NAME(a));
         print_poly(b, GET_NAME(b));
 
-        struct Poly c = add(a, b);
-        print_poly(c, GET_NAME(c));
-        struct Poly d = sub(a, b);
-        print_poly(d, GET_NAME(d));
-        struct Poly e = mul(a, b);
-        print_poly(e, GET_NAME(e));
-        struct Poly f = div(b, a, 0x7fffffff);
-        print_poly(f, GET_NAME(f));
+        struct Poly a_ADD_b = add(a, b);
+        print_poly(a_ADD_b, GET_NAME(a_ADD_b));
+        struct Poly a_SUB_b = sub(a, b);
+        print_poly(a_SUB_b, GET_NAME(a_SUB_b));
+        struct Poly a_MUL_b = mul(a, b);
+        print_poly(a_MUL_b, GET_NAME(a_MUL_b));
+        struct Poly b_DIV_a = div(b, a, 11);
+        print_poly(b_DIV_a, GET_NAME(b_DIV_a));
+        struct Poly a_DIV_b = div(a, b, 11);
+        print_poly(a_DIV_b, GET_NAME(a_DIV_b));
+
+        printf("-----------------------------------\n");
+        a = initPolyV(3, 1, 2, 3);
+        b = initPolyV(2, 1, 2);
+        print_poly(a, GET_NAME(a));
+        print_poly(b, GET_NAME(b));
+        a_DIV_b = div(a, b, 11);
+        print_poly(a_DIV_b, GET_NAME(a_DIV_b));
     }
     E4C_CATCH(RuntimeException)
     {
--- lib/poly.c
+++ lib/poly.c
@@ -1,16 +1,29 @@
 #include "poly.h"
 #include <stdarg.h>
 #include <string.h>
-#include "eea.h"
+#include "e4c_lite.h"
 
-#define max(a, b)               \
+#define MAX(a, b)               \
     ({                          \
         __typeof__(a) _a = (a); \
         __typeof__(b) _b = (b); \
         _a > _b ? _a : _b;      \
     })
 
-extern struct I_EX_GCD i_extended_euclidean(int a, int b);
+extern int inverse_i(int a, int module);
+
+const struct Poly ZERO = {.coef = {0}, .degree = 0};
+const struct Poly ONE = {.coef = {1}, .degree = 0};
+const struct Poly NOT_EXIST = {.coef = {0}, .degree = -1};
+
+static inline void fix_deg(struct Poly *target)
+{
+    for (int i = LEN - 1; i >= 0; i--)
+        if (target->coef[i]) {
+            target->degree = i;
+            break;
+        }
+}
 
 // init as zero
 void initPoly(struct Poly *target)
@@ -38,6 +51,7 @@ struct Poly add(struct Poly a, struct Poly b)
     struct Poly target = {.coef = {0}, .degree = len};
     for (int i = 0; i <= len; i++)
         target.coef[i] = a.coef[i] + b.coef[i];
+    fix_deg(&target);
     return target;
 }
 
@@ -47,6 +61,7 @@ struct Poly sub(struct Poly a, struct Poly b)
     struct Poly target = {.coef = {0}, .degree = len};
     for (int i = 0; i <= len; i++)
         target.coef[i] = a.coef[i] - b.coef[i];
+    fix_deg(&target);
     return target;
 }
 
@@ -65,26 +80,40 @@ struct Poly div(struct Poly a, struct Poly b, int module)
 {
     struct Poly quotient = {.coef = {0}, .degree = 0};
     int index = MAX(a.degree, b.degree);
-    if (index >= b.degree) {
+    if (a.degree >= b.degree) {
         /* a's degree is greather or equal b's degree */
-        for (int i = index; i <= b.degree; i--) {
-            unsigned int coef = a.coef[i] * inverse_i(b.coef[b.degree], module);
+        int inverse = inverse_i(b.coef[b.degree], module);
+        if (inverse == -1)
+            E4C_THROW(RuntimeException, "Inverse not found");
+        for (int i = index; i >= b.degree; i--) {
+            unsigned int coef = (a.coef[i] * inverse) % module;
             /* Simulate long division: a -= b * quotient[i] */
-            a = sub(a, mul(b, unitary(coef, i - b.degree)));
-            quotient = add(quotient, unitary(coef, i - b.degree));
+            a = mod(sub(a, mod(mul(b, unitary(coef, i - b.degree)), module)),
+                    module);
+            quotient = mod(add(quotient, unitary(coef, i - b.degree)), module);
         }
         fix_deg(&quotient);
-    } else
-        quotient = a; /* b's degree is greather than a's degree */
-
-    return mod(quotient, module);
+    }
+    return quotient;
 }
 
 struct Poly mod(struct Poly a, int module)
 {
-    for (int i = 0; i <= a.degree; i++)
-        a.coef[i] %= module;
+    for (int i = a.degree; i >= 0; i--)
+        a.coef[i] = (a.coef[i] + module) % module;
     return a;
+}
+
+bool equal(struct Poly a, struct Poly b)
+{
+    if (a.degree != b.degree)
+        return false;
+    else {
+        for (int i = 0; i <= b.degree; i++)
+            if (a.coef[i] - b.coef[i])
+                return false;
+        return true;
+    }
 }
 
 struct Poly unitary(int coef, int degree)
@@ -94,13 +123,4 @@ struct Poly unitary(int coef, int degree)
     result.coef[degree] = coef;
     result.degree = degree;
     return result;
-}
-
-static inline void fix_deg(struct Poly *target)
-{
-    for (int i = LEN - 1; i >= 0; i--)
-        if (target->coef[i]) {
-            target->degree = LEN - i;
-            break;
-        }
 }
