c8fb96f2cc3c946503a70599045f4b2dccc92cd9
25077667 <zxc25077667@gmail.com>
2020-11-29 00:38:32 +0800

    Add e4c lib

--- driver/testPoly.c
+++ driver/testPoly.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "../lib/e4c_lite.h"
 #include "../lib/eea.h"
 #include "../lib/poly.h"
+
 #define GET_NAME(var) #var
 
 void print_poly(const struct Poly p, const char *var_name)
@@ -18,23 +20,30 @@ extern struct Poly NOT_EXIST;
 
 int main(void)
 {
-    print_poly(ZERO, GET_NAME(ZERO));
-    print_poly(ONE, GET_NAME(ONE));
-    print_poly(NOT_EXIST, GET_NAME(NOT_EXIST));
+    E4C_TRY
+    {
+        print_poly(ZERO, GET_NAME(ZERO));
+        print_poly(ONE, GET_NAME(ONE));
+        print_poly(NOT_EXIST, GET_NAME(NOT_EXIST));
 
-    struct Poly a = {.coef = {1, 2, 3, 4, 5}, .degree = 4},
-                b = {.coef = {1, 2, 3, 4, 5, 6, 7, 8, 9}, .degree = 8};
-    print_poly(a, GET_NAME(a));
-    print_poly(b, GET_NAME(b));
+        struct Poly a = {.coef = {1, 2, 3, 4, 5}, .degree = 4},
+                    b = {.coef = {1, 2, 3, 4, 5, 6, 7, 8, 9}, .degree = 8};
+        print_poly(a, GET_NAME(a));
+        print_poly(b, GET_NAME(b));
 
-    struct Poly c = add(a, b);
-    print_poly(c, GET_NAME(c));
-    struct Poly d = sub(a, b);
-    print_poly(d, GET_NAME(d));
-    struct Poly e = mul(a, b);
-    print_poly(e, GET_NAME(e));
-    struct Poly f = div(b, a, 0x7fffffff);
-    print_poly(f, GET_NAME(f));
+        struct Poly c = add(a, b);
+        print_poly(c, GET_NAME(c));
+        struct Poly d = sub(a, b);
+        print_poly(d, GET_NAME(d));
+        struct Poly e = mul(a, b);
+        print_poly(e, GET_NAME(e));
+        struct Poly f = div(b, a, 0x7fffffff);
+        print_poly(f, GET_NAME(f));
+    }
+    E4C_CATCH(RuntimeException)
+    {
+        printf("Error: %s\n", E4C_EXCEPTION.type->name);
+    }
 
     return 0;
 }
